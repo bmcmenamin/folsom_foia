64b687f68e8e958bb9fdda25ffb1d6a5b0292518
bmcmenamin <bmcmenamin@gmail.com>
2018-08-22 00:02:45 -0400

    updating real assets

---
+++ story.js
@@ -0,0 +1,48 @@
+const PARAMS = {
+    "container": "#scroll",
+    "text": {
+        "rgb_str": "255,255,255,",
+        "fade_position": 0.85
+    },
+    "caption": {
+        "rgb_str": "255,255,255,",
+        "vert_margin": 4,
+        "fade_position": 0.15
+    },
+    "chart": {
+        "path": "./assets/img/",
+        "hor_margin": 8,
+        "fade_position": 0.08
+    }
+}
+
+const STORY = [
+    {
+        "step": "50632\nMATLOCK",
+        "chart": "A50632/control_card.png",
+        "alt": "Prison control card for prisoner A-50632 MATLOCK, James Wesley",
+        "caption": "Prisoner A-50632 was named James Wesley Matlock. Born in 1938 Arkansas, he entered prison at age 20 with a life sentence stemming from a variety of charges. Robbery (1st degree), assault with a deadly weapon, and kidnapping."
+    },
+    {
+        "step": "39879\nBATCHELDER",
+        "chart": "A39879/control_card.png",
+        "alt": "Prison control card for prisoner A-39879 BATCHELDER, Jack",
+        "caption": "Prisoner A-50632 was named Jack Batchelder. Born in 1934 California, he entered prison at age 22 with a for a 6mo to 10 year sentence."
+    },
+    {
+        "step": "August\n1955",
+        "chart": "",
+        "caption": "Lorem ipsum dolor sit amet, ex stet dissentiet reprehendunt cum. Vis prima similique contentiones ut, est legere vivendum at, te elitr apeirian interpretaris sea. Vel postea fierent te, quo prima copiosae suavitate ad. Populo postea at eum, quo ex inimicus accusamus. Sit scripserit persequeris consequuntur no. Impedit lucilius eu nam, ex sea justo oporteat, te eam labore doctus deleniti."
+    },
+    {
+        "step": "Septembruary\n1955",
+        "chart": "bab2.jpg",
+        "alt": ":(",
+        "caption": "Lorem ipsum dolor sit amet, ex stet dissentiet reprehendunt cum. Vis prima similique contentiones ut, est legere vivendum at, te elitr apeirian interpretaris sea. Vel postea fierent te, quo prima copiosae suavitate ad. Populo postea at eum, quo ex inimicus accusamus. Sit scripserit persequeris consequuntur no. Impedit lucilius eu nam, ex sea justo oporteat, te eam labore doctus deleniti."
+    },
+    {
+        "step": "Thermidor\n1955",
+        "chart": "bab.jpg",
+        "caption": "Lorem ipsum dolor sit amet, ex stet dissentiet reprehendunt cum. Vis prima similique contentiones ut, est legere vivendum at, te elitr apeirian interpretaris sea. Vel postea fierent te, quo prima copiosae suavitate ad. Populo postea at eum, quo ex inimicus accusamus. Sit scripserit persequeris consequuntur no. Impedit lucilius eu nam, ex sea justo oporteat, te eam labore doctus deleniti."
+    }
+]
