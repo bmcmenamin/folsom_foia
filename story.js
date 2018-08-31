8b7f6118a6de4fc97c1fd6c6828f47a13c95d48e
bmcmenamin <bmcmenamin@gmail.com>
2018-08-30 21:09:16 -0400

    story updates

--- story.js
+++ story.js
@@ -4,18 +4,19 @@ const STATIC_TEXT = {
         "These men have receptions..."
     ],
     "intro_paras": [
-        "The morning of January 1968, Johnny Cash gave two live performances for the inmates at Folsom Prison. The recordings from those shows were released to critical acclaim later that year as the album <a href='https://open.spotify.com/embed/album/4TJIdlY9hGSSTO1kUs1neh'><i>At Folsom Prison</i></a>. Cash's hand-written liner-notes describes the unique atmosphere of this record: <i>\"Listen closely to this album and you hear in the background the clanging of the doors, the shrill of the whistle, the shout of the men -- even laughter from men who had forgetten how to laugh\"</i>.",
-        "One moment that had always stood out to me was immediately after <i>Cocaine Blues</i>. The warden interrupted the performance for an official announcement : <i>\"These men have receptions: Matlock A50632 and Batchelder A39879\"</i>. These two inmates were pulled out of a concert would become one of the <a href='https://pitchfork.com/features/lists-and-guides/the-200-best-albums-of-the-1960s/?page=8'>greatest albums</a> <a href='https://www.rollingstone.com/music/music-lists/500-greatest-albums-of-all-time-156826/johnny-cash-at-folsom-prison-152260/'>of all time</a> because they happened to get visitors at that moment.",
-        "I was curious who these men were, so I sent FOIA requests across the state of California to see what I could discover. Eventually, I received copied of prison records for James Wesley Matlock (A50632) and Jack Batchelder (A39879). Below I walk summarize their experience with the penal system, as told through prison records."
+        "In January 1968, Johnny Cash recorded two live performances given for inmates at Folsom Prison. Those recordings were released as the album <a href='https://en.wikipedia.org/wiki/At_Folsom_Prison'><i>At Folsom Prison</i></a> to critical acclaim later that year. The acclaim was, in part, due to the unique atmosphere of the album. According to Cash's hand-written liner notes: <i>\"Listen closely to this album and you hear in the background the clanging of the doors, the shrill of the whistle, the shout of the men -- even laughter from men who had forgetten how to laugh\"</i>.",
+        "One of those background moments that stands out is when the warden interrupts the performance for an official announcement immediately after <i>Cocaine Blues</i>: <i>\"These men have receptions: Matlock A50632 and Batchelder A39879.\"</i>. These two men -- Matlock and Batchelder -- were pulled out of a concert that would go on to become one of the <a href='https://pitchfork.com/features/lists-and-guides/the-200-best-albums-of-the-1960s/?page=8'>greatest albums</a> <a href='https://www.rollingstone.com/music/music-lists/500-greatest-albums-of-all-time-156826/johnny-cash-at-folsom-prison-152260/'>of all time</a>. I had always wanted to know more -- Who were these guys? Who were their vistiors?",
+        "I sent FOIA requests to California, and eventually I was mailed the prison records for James Wesley Matlock (A50632) and Jack Batchelder (A39879). Below I summarize their experience with Folsom Prison by walking through their prison records."
     ],
     "outro_paras": [
-        "There's not really a conclusion to draw here other than the fact that Matlock and Batchelder were real men in Folsom Prison, and not set-dressing to give the album a sense of authenticity. It's easy to notice parallels between their experiences and the lyrics of <i>At Folsom Prison</i>, but I'll leave that speculation up to you.",
+        "There's not really a conclusion to draw here other than the fact that Matlock and Batchelder were real men in Folsom Prison, and not set-dressing to give the album a sense of authenticity like some of the <a href='https://www.denverpost.com/2008/12/10/a-cash-secret-unlocked/'>other crowd noise</a>. It's easy to notice parallels between their lives and Cash's lyrics, but I don't want to start speculating too much about men I've only known through reproductions of water-damaged government records.",
         "If you'd like a copy of their records, here are some links so you don't have to file a FOIA request:"
           +"<ul><a href='raw_data/pdf/A39879_control_card.pdf'>Batchelder's control card (1 page)</a></ul>"
           +"<ul><a href='raw_data/pdf/A39879_record_card.pdf'>Batchelder's record card (1 page)</a></ul>"
           +"<ul><a href='raw_data/pdf/CDC_A39879_discharge_case_file.pd.pdf'>Batchelder's discharge case file (116 pages)</a></ul>"
           +"<ul><a href='raw_data/pdf/A50632_control_card.pdf'>Matlock'scontrol card (1 page)</a></ul>"
-          +"<ul><a href='raw_data/pdf/CDC_A50632_discharge_case_file.pd.pdf'>Matlock'sdischarge case file (52 pages)</a></ul>"
+          +"<ul><a href='raw_data/pdf/CDC_A50632_discharge_case_file.pd.pdf'>Matlock'sdischarge case file (52 pages)</a></ul>",
+        "I still don't know who their visitors were that day."
     ]
 }
 
@@ -31,7 +32,7 @@ const STORY = [
         "step": "BATCHELDER\nPrison Record",
         "chart": "A39879/record_card.png",
         "alt": "Prison record card for prisoner A-39879 BATCHELDER, Jack",
-        "caption": "Jack Batchelder was in an out of prison for most of his adult life. The barely-legible section in the upper right of his official prison records indicaes when he was paroled and re-enrolled in state prison up to his death on June 9th, 1972. I'll go over that later, so for now, just take note of his extensive tattoo collection that included: Several nude female figures, \"KKK\", some dancing legs with a top hat, \"Mi Vida Loca\", and a scar from one tattoo that he regretted."
+        "caption": "Jack Batchelder was in an out of prison for most of his adult life. The barely-legible section in the upper right of his official prison records indicates that he was paroled and re-enrolled in state prison several times up until his death on June 9th, 1972.\nMore interestingly, take note of his extensive tattoo collection: Several nude female figures, \"KKK\", some dancing legs with a top hat, \"Mi Vida Loca\", and a scar from a tattoo that he actually regretted."
     },
     {
         "step": "BATCHELDER - 1957\nGrand Theft Scrap metal",
@@ -55,25 +56,25 @@ const STORY = [
         "step": "BATCHELDER - 1963\nAttemped Escape",
         "chart": "A39879/1963/report_case_8383.png",
         "alt": "Jack Batchelder case 8383 report",
-        "caption": "... and while awaiting trial for the robbery, he attempted to escape from the courthouse by using bedsheets to make a rope and climbing out the window."
+        "caption": "... and while awaiting trial for the robbery, he attempted to escape from the courthouse by using bedsheets to make a rope and climb out the window."
     },
     {
         "step": "BATCHELDER - 1963\nAttemped Escape, again",
         "chart": "A39879/1963/report_case_8486.png",
         "alt": "Jack Batchelder case 8486 report",
-        "caption": "... and then jail he tried to escape again. This time he crawled through an air duct to the roof and used a rope made from mattress covers to climb down from the roof. They found him two days later at his mother's house. He didn't try to escape anymore."
+        "caption": "... and then jail he tried to escape by crawling to the roof through an air duct and using a rope of mattress covers to climb down. They found him two days later at his mother's house."
     },
     {
         "step": "BATCHELDER - 1967\nA too-honest parole hearing",
         "chart": "A39879/1967/parole_report.png",
         "alt": "Jack Batchelder 1967 parole report",
-        "caption": "A few years later, back in prison, he had parole board meeting. Seeing that he'd been paroled multiple times in the past, you would think he was good at it. But... he told that he commits crime because there's often \"nothing better to do at the time\", and \"If you're going to be bad, do a good job of it, and live it up\". He did not get parole. But on the bright side, that meant he was around for half of Johnny Cash's concert eight months later."
+        "caption": "A few years later, back in prison, he had parole board meeting. Seeing that he'd been given parole multiple times in the past, you think he'd know what they wanted to hear. But... he told that he commits crime because there's often \"nothing better to do at the time\", and \"If you're going to be bad, do a good job of it, and live it up\". He didn't get parole, but on the bright side, he got to stick around prison to hear half of Johnny Cash's concert eight months later."
     },
     {
         "step": "BATCHELDER - 1972\nStabbed to death",
         "chart": "A39879/1972/death_certificate.png",
         "alt": "Death certificate for prisoner A-39879 BATCHELDER, Jack",
-        "caption": "Jack Batchelder was killed in prison by multiple stab wounds in the jugular."
+        "caption": "Several years later, Jack Batchelder was killed in prison when an unknown individual stabbed him multiple times in the jugular."
     },
 
     {
@@ -98,7 +99,7 @@ const STORY = [
         "step": "MATLOCK - 1959\nPolice Report",
         "chart": "A50632/1959/police_report.png",
         "alt": "Police report for James Matlock's 1959 Armed Robbery",
-        "caption": "The official police report states that Matlock was one of three men that held up a bar in Berkely. The assualt with a deadly weapon was due to the use of 'pistol-whipping' and 'judo-chopping'. The kidnapping was because they tied somebody up."
+        "caption": "The official police report states that Matlock was one of three men that held up a bar in Berkeley. The assualt with a deadly weapon was due to the use of 'pistol-whipping' and 'judo-chopping'. The kidnapping was because they tied somebody up. All for less than $250."
     },
     {
         "step": "MATLOCK - 1959\nHis Report",
@@ -110,43 +111,43 @@ const STORY = [
         "step": "MATLOCK - 1965\nParoled",
         "chart": "A50632/1965/parole_notes.png",
         "alt": "1965 Matlock released on Parole",
-        "caption": "In a parole hearing six years later, he seems to have given some serious thought as to mhy he pistol-whipped the man he tied up. And he thinks that he'd probably be a janitor -- maybe a construction worker -- if he were paroled.  Parole: GRANTED."
+        "caption": "Six years later, he was up for parole. He had meaningfully introspected why he did what he did, and had plans to be a janitor -- maybe a construction worker -- if he were paroled.  Parole: GRANTED."
     },
     {
         "step": "MATLOCK - 1966\nUn-Paroled",
         "chart": "A50632/1966/parole_violation.png",
         "alt": "1966 Matlock violates Parole",
-        "caption": "... but he wasn't out very long before he gets brough back in for violating the terms of parole and holding up a service station."
+        "caption": "He wasn't out very long before things went wrong and he was brought back for violating multiple terms of his parole: leaving the county, driving a car, holding up a service station, and stealing a car. Presumably he drove the stolen car as well, but they didn't bring that part up."
     },
     {
         "step": "MATLOCK - 1966\nHis account",
         "chart": "A50632/1966/his_account.png",
         "alt": "1966 Matlock violates Parole",
-        "caption": "Why did he hold up a service station? Well, he was on a Las Vegas road trip with his friend and got left behind when he went to use the bathroom. Holding up the service station with a pretend finger-gun just seemed like the easiest way to get back to his girlfriend. "
+        "caption": "So why did he hold up a service station? Well, he was on a Las Vegas road trip with his friend and got left behind when he went to use the bathroom. Holding up the service station with a pretend finger-gun just seemed like the easiest way to get back home to his girlfriend. "
     },
     {
         "step": "MATLOCK - 1972\nParole Hearing",
         "chart": "A50632/1972/career_plans.png",
         "alt": "1972 Matlock Parole Hearing",
-        "caption": "Another six years pass, and Matlock's in another parole hearing. Now when he's asked about what type of work he'd like, now he's thinking that a body shop would be a better fit than construction. Parole: DENIED."
+        "caption": "Matlock has another six years later. Now when he's asked about what type of work he'd like, he's thinking that a body shop would be a better fit than janitorial or construction work. Parole: DENIED."
     },
     {
         "step": "MATLOCK - 1973\nParole Hearing",
         "chart": "A50632/1973/career_plans.png",
         "alt": "1973 Matlock Parole Hearing",
-        "caption": "Over the next year, he starts working in a serology lab and a University researcher says that it's a potential line of work for him if he were to get out. Parole: DENIED."
+        "caption": "In the next year he starts working in a serology lab and a University researcher says that it might be a good potential line of work for him. He told the parole board. Parole: DENIED."
      },
     {
         "step": "MATLOCK - 1974\nParole Hearing",
         "chart": "A50632/1974/parole_report.png",
         "alt": "1974 Matlock Parole Hearing",
-        "caption": "Another year, another parole hearing. Now he's found religion and has doubled-down on his interest in being a lab tech. The researcher running his Serology lab will help him find a job. Parole: GRANTED."
+        "caption": "Another year passes, and he's in another parole hearing. Now he's found religion and has doubled-down on his interest in becoming a lab tech. The researcher running the Serology lab says that they'll help him find a job. Parole: GRANTED."
     },
     {
         "step": "50632: JAMES MATLOCK",
         "chart": "A50632/1975/parole_agreement.png",
         "alt": "1975 Parole Agreement for James Matlock",
-        "caption": "In the summer 1975, James Matlock was approved for another parole and there are no additional records. I assume that this is a happy ending."
+        "caption": "In the summer 1975, James Matlock was approved for another parole and there are no additional records. I assume that this is a happy ending and he became a lab technician."
     }
 ]
 
